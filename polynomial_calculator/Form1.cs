52868652b13883729eb06927513f9ba22318a9a7
Wiktor Samsel-Baudat <73764933+wsamselbaudat@users.noreply.github.com>
2023-04-03 21:43:06 +0200

    SerializacjaJSON
    Serializacje do pliku i z Pliku, pozdrawiam cieplutko

--- polynomial_calculator/Form1.cs
+++ polynomial_calculator/Form1.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+
 
 namespace polynomial_calculator
 {
@@ -19,11 +26,13 @@ namespace polynomial_calculator
         public Form1()
         {
             InitializeComponent();
+            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void AdditionEvent(object sender, EventArgs e)
@@ -64,6 +73,34 @@ namespace polynomial_calculator
 
         private void secondPolynominalText_TextChanged(object sender, EventArgs e)
         {
+            
+        }
+
+        private void firstPolynominalText_TextChanged(object sender, EventArgs e)
+        {
+            
+
+            
+        }
+
+        private void resultLabel_Click(object sender, EventArgs e)
+        {
+            string result = resultLabel.Text;
+
+           //serialize result to json
+            string json = JsonConvert.SerializeObject(result, Formatting.Indented); 
+            File.WriteAllText("result.json", json);
+
+            string fullPath = Path.GetFullPath("result.json");
+            File.WriteAllText(fullPath, json);
+
+            Console.WriteLine($"Zapisano plik {"result.json"} w {fullPath}");
+            //deserialize json to object
+            string jsonFromFile = File.ReadAllText("result.json");
+            string resultFromFile = JsonConvert.DeserializeObject<string>(jsonFromFile);
+
+            //display results in label2 
+            label2.Text = resultFromFile;
 
         }
     }
