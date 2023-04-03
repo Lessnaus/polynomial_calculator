233e6c4408c1b00c86d0e5d3699258b879dc8d36
Lessnaus <lessnau001@gmail.com>
2023-04-03 17:30:02 +0200

    first_commit

---
+++ polynomial_calculator/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace polynomial_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.firstPolynominalText = new System.Windows.Forms.TextBox();
+            this.secondPolynominalText = new System.Windows.Forms.TextBox();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(3, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(402, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "POLYNOMIAL CALCULATOR";
+            // 
+            // firstPolynominalText
+            // 
+            this.firstPolynominalText.Location = new System.Drawing.Point(9, 171);
+            this.firstPolynominalText.Name = "firstPolynominalText";
+            this.firstPolynominalText.Size = new System.Drawing.Size(170, 20);
+            this.firstPolynominalText.TabIndex = 1;
+            // 
+            // secondPolynominalText
+            // 
+            this.secondPolynominalText.Location = new System.Drawing.Point(221, 171);
+            this.secondPolynominalText.Name = "secondPolynominalText";
+            this.secondPolynominalText.Size = new System.Drawing.Size(170, 20);
+            this.secondPolynominalText.TabIndex = 2;
+            // 
+            // plusButton
+            // 
+            this.plusButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.plusButton.Location = new System.Drawing.Point(116, 223);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(30, 30);
+            this.plusButton.TabIndex = 3;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.AdditionEvent);
+            // 
+            // minusButton
+            // 
+            this.minusButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.minusButton.Location = new System.Drawing.Point(165, 223);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(30, 30);
+            this.minusButton.TabIndex = 4;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = true;
+            this.minusButton.Click += new System.EventHandler(this.SubtractionEvent);
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.multiplyButton.Location = new System.Drawing.Point(213, 223);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(30, 30);
+            this.multiplyButton.TabIndex = 5;
+            this.multiplyButton.Text = "*";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.MultiplycationEvent);
+            // 
+            // divideButton
+            // 
+            this.divideButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.divideButton.Location = new System.Drawing.Point(260, 223);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(30, 30);
+            this.divideButton.TabIndex = 6;
+            this.divideButton.Text = "/";
+            this.divideButton.UseVisualStyleBackColor = true;
+            this.divideButton.Click += new System.EventHandler(this.DivisionEvent);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.resultLabel.Location = new System.Drawing.Point(172, 299);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(61, 20);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Text = "Wynik:";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(403, 465);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.secondPolynominalText);
+            this.Controls.Add(this.firstPolynominalText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox firstPolynominalText;
+        private System.Windows.Forms.TextBox secondPolynominalText;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button divideButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ polynomial_calculator/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polynomial_calculator
+{
+    public partial class Form1 : Form
+    {
+
+        string poly1;
+        string poly2;
+        string result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdditionEvent(object sender, EventArgs e)
+        {
+            poly1 = firstPolynominalText.Text;
+            poly2 = secondPolynominalText.Text;
+
+            string[] poly1Terms = poly1.Split('+', '-');
+            string[] poly2Terms = poly2.Split('+', '-');
+
+            int[] poly1Coeffs = new int[poly1Terms.Length];
+            int[] poly2Coeffs = new int[poly2Terms.Length];
+
+            for (int i = 0; i < poly1Terms.Length; i++)
+            {
+                string term = poly1Terms[i].Trim();
+
+                if (term == "") continue;
+
+                int coef, exp;
+                ParseTerm(term, out coef, out exp);
+
+                if (poly1Terms[i][0] == '-')
+                {
+                    coef = -coef;
+                }
+
+                poly1Coeffs[exp] = coef;
+            }
+
+            for (int i = 0; i < poly2Terms.Length; i++)
+            {
+                string term = poly2Terms[i].Trim();
+
+                if (term == "") continue;
+
+                int coef, exp;
+                ParseTerm(term, out coef, out exp);
+
+                if (poly2Terms[i][0] == '-')
+                {
+                    coef = -coef;
+                }
+
+                poly2Coeffs[exp] = coef;
+            }
+
+            int[] resultCoeffs = new int[Math.Max(poly1Coeffs.Length, poly2Coeffs.Length)];
+
+            for (int i = 0; i < resultCoeffs.Length; i++)
+            {
+                int coef1 = (i < poly1Coeffs.Length) ? poly1Coeffs[i] : 0;
+                int coef2 = (i < poly2Coeffs.Length) ? poly2Coeffs[i] : 0;
+
+                resultCoeffs[i] = coef1 + coef2;
+            }
+
+            for (int i = resultCoeffs.Length - 1; i >= 0; i--)
+            {
+                if (resultCoeffs[i] == 0) continue;
+
+                if (resultCoeffs[i] > 0 && result != "")
+                {
+                    result += "+";
+                }
+
+                result += resultCoeffs[i].ToString();
+
+                if (i > 0)
+                {
+                    result += "x";
+                }
+
+                if (i > 1)
+                {
+                    result += "^" + i.ToString();
+                }
+            }
+
+
+
+            resultLabel.Text = "Wynik: " + Environment.NewLine + result;
+        }
+
+        private void SubtractionEvent(object sender, EventArgs e)
+        {
+            poly1 = firstPolynominalText.Text;
+            poly2 = secondPolynominalText.Text;
+
+            resultLabel.Text = "Wynik: " + Environment.NewLine + result;
+        }
+
+        private void MultiplycationEvent(object sender, EventArgs e)
+        {
+            poly1 = firstPolynominalText.Text;
+            poly2 = secondPolynominalText.Text;
+
+            resultLabel.Text = "Wynik: " + Environment.NewLine + result;
+        }
+
+        private void DivisionEvent(object sender, EventArgs e)
+        {
+            poly1 = firstPolynominalText.Text;
+            poly2 = secondPolynominalText.Text;
+
+
+
+
+            resultLabel.Text = "Wynik: " + Environment.NewLine + result;
+        }
+        private void Parse(string expression)
+        {
+            string[] terms = expression.Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string term in terms)
+            {
+                double coefficient;
+                int exponent;
+                if (double.TryParse(term.Trim(' ', '+', '-'), out coefficient))
+                {
+                    if (term.Contains('x'))
+                    {
+                        exponent = GetExponent(term);
+                        if (coefficients.ContainsKey(exponent))
+                        {
+                            coefficients[exponent] += coefficient;
+                        }
+                        else
+                        {
+                            coefficients[exponent] = coefficient;
+                        }
+                    }
+                    else
+                    {
+                        if (coefficients.ContainsKey(0))
+                        {
+                            coefficients[0] += coefficient;
+                        }
+                        else
+                        {
+                            coefficients[0] = coefficient;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
