3bc14313c7230513c3ddd0687849ebe441fd7b91
Lessnaus <lessnau001@gmail.com>
2023-04-03 19:10:41 +0200

    delete

--- polynomial_calculator/Form1.cs
+++ polynomial_calculator/Form1.cs
@@ -31,80 +31,6 @@ namespace polynomial_calculator
             poly1 = firstPolynominalText.Text;
             poly2 = secondPolynominalText.Text;
 
-            string[] poly1Terms = poly1.Split('+', '-');
-            string[] poly2Terms = poly2.Split('+', '-');
-
-            int[] poly1Coeffs = new int[poly1Terms.Length];
-            int[] poly2Coeffs = new int[poly2Terms.Length];
-
-            for (int i = 0; i < poly1Terms.Length; i++)
-            {
-                string term = poly1Terms[i].Trim();
-
-                if (term == "") continue;
-
-                int coef, exp;
-                ParseTerm(term, out coef, out exp);
-
-                if (poly1Terms[i][0] == '-')
-                {
-                    coef = -coef;
-                }
-
-                poly1Coeffs[exp] = coef;
-            }
-
-            for (int i = 0; i < poly2Terms.Length; i++)
-            {
-                string term = poly2Terms[i].Trim();
-
-                if (term == "") continue;
-
-                int coef, exp;
-                ParseTerm(term, out coef, out exp);
-
-                if (poly2Terms[i][0] == '-')
-                {
-                    coef = -coef;
-                }
-
-                poly2Coeffs[exp] = coef;
-            }
-
-            int[] resultCoeffs = new int[Math.Max(poly1Coeffs.Length, poly2Coeffs.Length)];
-
-            for (int i = 0; i < resultCoeffs.Length; i++)
-            {
-                int coef1 = (i < poly1Coeffs.Length) ? poly1Coeffs[i] : 0;
-                int coef2 = (i < poly2Coeffs.Length) ? poly2Coeffs[i] : 0;
-
-                resultCoeffs[i] = coef1 + coef2;
-            }
-
-            for (int i = resultCoeffs.Length - 1; i >= 0; i--)
-            {
-                if (resultCoeffs[i] == 0) continue;
-
-                if (resultCoeffs[i] > 0 && result != "")
-                {
-                    result += "+";
-                }
-
-                result += resultCoeffs[i].ToString();
-
-                if (i > 0)
-                {
-                    result += "x";
-                }
-
-                if (i > 1)
-                {
-                    result += "^" + i.ToString();
-                }
-            }
-
-
-
             resultLabel.Text = "Wynik: " + Environment.NewLine + result;
         }
 
@@ -134,41 +60,6 @@ namespace polynomial_calculator
 
             resultLabel.Text = "Wynik: " + Environment.NewLine + result;
         }
-        private void Parse(string expression)
-        {
-            string[] terms = expression.Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string term in terms)
-            {
-                double coefficient;
-                int exponent;
-                if (double.TryParse(term.Trim(' ', '+', '-'), out coefficient))
-                {
-                    if (term.Contains('x'))
-                    {
-                        exponent = GetExponent(term);
-                        if (coefficients.ContainsKey(exponent))
-                        {
-                            coefficients[exponent] += coefficient;
-                        }
-                        else
-                        {
-                            coefficients[exponent] = coefficient;
-                        }
-                    }
-                    else
-                    {
-                        if (coefficients.ContainsKey(0))
-                        {
-                            coefficients[0] += coefficient;
-                        }
-                        else
-                        {
-                            coefficients[0] = coefficient;
-                        }
-                    }
-                }
-            }
-        }
+      
     }
 }
