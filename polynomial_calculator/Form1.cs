4a58660c598a72450b8ba40392ecaabdf9fd7383
Lessnaus <lessnau001@gmail.com>
2023-04-05 17:00:55 +0200

    Chęci do życia +1

--- polynomial_calculator/Form1.cs
+++ polynomial_calculator/Form1.cs
@@ -12,17 +12,15 @@ using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using System.IO;
-
-
+using System.Text.RegularExpressions;
+using ExtendedArithmetic;
 
 namespace polynomial_calculator
 {
     public partial class Form1 : Form
     {
 
-        string poly1;
-        string poly2;
-        string result;
+
         public Form1()
         {
             InitializeComponent();
@@ -37,75 +35,233 @@ namespace polynomial_calculator
         private void AdditionEvent(object sender, EventArgs e)
         {
             // Pobierz łańcuchy znaków reprezentujące dwa wielomiany
-            string poly1 = firstPolynominalText.Text.Trim();
-            string poly2 = secondPolynominalText.Text.Trim();
-
-            // Stwórz listę obiektów Term dla każdego wielomianu
-            List<Term> terms1 = ParsePolynomial(poly1);
-            List<Term> terms2 = ParsePolynomial(poly2);
-
-            // Zsumuj wielomiany
-            List<Term> result = AddPolynomials(terms1, terms2);
-
-            // Konwertuj wynik na łańcuch i wyświetl go w kontrolce tekstowej
-            string resultStr = ConvertPolynomialToString(result);
-            Console.WriteLine(result);
 
 
+            string wielomian1 = firstPolynominalText.Text;
+            string outputString1 = "";
 
-            string json = JsonConvert.SerializeObject(result, Formatting.Indented);
-            File.WriteAllText("result.json", json);
-
-            string fullpath = Path.GetFullPath("result.json");
-            File.WriteAllText(fullpath, json);
-
-            Console.WriteLine($"zapisano plik {"result.json"} w {fullpath}");
-            //deserialize json to object
-            string jsonfromfile = File.ReadAllText("result.json");
-
-            try
+            for (int i = 0; i < wielomian1.Length; i++)
             {
-                string resultfromfile = JsonConvert.DeserializeObject<string>(jsonfromfile);
+                if (wielomian1[i] == 'x')
+                {
+                    outputString1 += "*x";
+                }
+                else if (wielomian1[i] == '-' && (i == 0 || wielomian1[i - 1] == '+'))
+                {
+                    outputString1 += "-1";
+                }
+                else if (wielomian1[i] == '+' || wielomian1[i] == '-')
+                {
+                    outputString1 += wielomian1[i];
+                }
+                else
+                {
+                    outputString1 += wielomian1[i];
+                }
             }
-            catch (Exception)
+            string wielomian2 = secondPolynominalText.Text;
+            string outputString2 = "";
+
+            for (int i = 0; i < wielomian2.Length; i++)
             {
-                label2.Text = "nie można znaleźć pliku/załadować historii";
+                if (wielomian2[i] == 'x')
+                {
+                    outputString2 += "*x";
+                }
+                else if (wielomian2[i] == '-' && (i == 0 || wielomian2[i - 1] == '+'))
+                {
+                    outputString2 += "-1";
+                }
+                else if (wielomian2[i] == '+' || wielomian2[i] == '-')
+                {
+                    outputString2 += wielomian2[i];
+                }
+                else
+                {
+                    outputString2 += wielomian2[i];
+                }
             }
+            ExtendedArithmetic.Polynomial asd = Polynomial.Parse(outputString1);
+            ExtendedArithmetic.Polynomial assd = Polynomial.Parse(outputString2);
 
+            Polynomial wynik = ExtendedArithmetic.Polynomial.Add(asd, assd);
+            String qwe = wynik.ToString();
 
-            //display result in label2;
-            //label2.Text = ResultFromFile;
-
-
-
-            resultLabel.Text = "Wynik: " + Environment.NewLine + resultStr;
+            resultLabel.Text = "Wynik: " + qwe;
         }
 
         private void SubtractionEvent(object sender, EventArgs e)
         {
-            poly1 = firstPolynominalText.Text;
-            poly2 = secondPolynominalText.Text;
+            // Pobierz łańcuchy znaków reprezentujące dwa wielomiany
 
-            resultLabel.Text = "Wynik: " + Environment.NewLine + result;
+
+            string wielomian1 = firstPolynominalText.Text;
+            string outputString1 = "";
+
+            for (int i = 0; i < wielomian1.Length; i++)
+            {
+                if (wielomian1[i] == 'x')
+                {
+                    outputString1 += "*x";
+                }
+                else if (wielomian1[i] == '-' && (i == 0 || wielomian1[i - 1] == '+'))
+                {
+                    outputString1 += "-1";
+                }
+                else if (wielomian1[i] == '+' || wielomian1[i] == '-')
+                {
+                    outputString1 += wielomian1[i];
+                }
+                else
+                {
+                    outputString1 += wielomian1[i];
+                }
+            }
+            string wielomian2 = secondPolynominalText.Text;
+            string outputString2 = "";
+
+            for (int i = 0; i < wielomian2.Length; i++)
+            {
+                if (wielomian2[i] == 'x')
+                {
+                    outputString2 += "*x";
+                }
+                else if (wielomian2[i] == '-' && (i == 0 || wielomian2[i - 1] == '+'))
+                {
+                    outputString2 += "-1";
+                }
+                else if (wielomian2[i] == '+' || wielomian2[i] == '-')
+                {
+                    outputString2 += wielomian2[i];
+                }
+                else
+                {
+                    outputString2 += wielomian2[i];
+                }
+            }
+            ExtendedArithmetic.Polynomial asd = Polynomial.Parse(outputString1);
+            ExtendedArithmetic.Polynomial assd = Polynomial.Parse(outputString2);
+
+            Polynomial wynik = ExtendedArithmetic.Polynomial.Subtract(asd, assd);
+            String qwe = wynik.ToString();
+
+            resultLabel.Text = "Wynik: " + qwe;
         }
 
         private void MultiplycationEvent(object sender, EventArgs e)
         {
-            poly1 = firstPolynominalText.Text;
-            poly2 = secondPolynominalText.Text;
+            // Pobierz łańcuchy znaków reprezentujące dwa wielomiany
 
-            resultLabel.Text = "Wynik: " + Environment.NewLine + result;
+
+            string wielomian1 = firstPolynominalText.Text;
+            string outputString1 = "";
+
+            for (int i = 0; i < wielomian1.Length; i++)
+            {
+                if (wielomian1[i] == 'x')
+                {
+                    outputString1 += "*x";
+                }
+                else if (wielomian1[i] == '-' && (i == 0 || wielomian1[i - 1] == '+'))
+                {
+                    outputString1 += "-1";
+                }
+                else if (wielomian1[i] == '+' || wielomian1[i] == '-')
+                {
+                    outputString1 += wielomian1[i];
+                }
+                else
+                {
+                    outputString1 += wielomian1[i];
+                }
+            }
+            string wielomian2 = secondPolynominalText.Text;
+            string outputString2 = "";
+
+            for (int i = 0; i < wielomian2.Length; i++)
+            {
+                if (wielomian2[i] == 'x')
+                {
+                    outputString2 += "*x";
+                }
+                else if (wielomian2[i] == '-' && (i == 0 || wielomian2[i - 1] == '+'))
+                {
+                    outputString2 += "-1";
+                }
+                else if (wielomian2[i] == '+' || wielomian2[i] == '-')
+                {
+                    outputString2 += wielomian2[i];
+                }
+                else
+                {
+                    outputString2 += wielomian2[i];
+                }
+            }
+            ExtendedArithmetic.Polynomial asd = Polynomial.Parse(outputString1);
+            ExtendedArithmetic.Polynomial assd = Polynomial.Parse(outputString2);
+
+            Polynomial wynik = ExtendedArithmetic.Polynomial.Multiply(asd, assd);
+            String qwe = wynik.ToString();
+
+            resultLabel.Text = "Wynik: " + qwe;
+
         }
 
         private void DivisionEvent(object sender, EventArgs e)
-        {
-            poly1 = firstPolynominalText.Text;
-            poly2 = secondPolynominalText.Text;
+        {// Pobierz łańcuchy znaków reprezentujące dwa wielomiany
 
 
+            string wielomian1 = firstPolynominalText.Text;
+            string outputString1 = "";
 
+            for (int i = 0; i < wielomian1.Length; i++)
+            {
+                if (wielomian1[i] == 'x')
+                {
+                    outputString1 += "*x";
+                }
+                else if (wielomian1[i] == '-' && (i == 0 || wielomian1[i - 1] == '+'))
+                {
+                    outputString1 += "-1";
+                }
+                else if (wielomian1[i] == '+' || wielomian1[i] == '-')
+                {
+                    outputString1 += wielomian1[i];
+                }
+                else
+                {
+                    outputString1 += wielomian1[i];
+                }
+            }
+            string wielomian2 = secondPolynominalText.Text;
+            string outputString2 = "";
 
-            resultLabel.Text = "Wynik: " + Environment.NewLine + result;
+            for (int i = 0; i < wielomian2.Length; i++)
+            {
+                if (wielomian2[i] == 'x')
+                {
+                    outputString2 += "*x";
+                }
+                else if (wielomian2[i] == '-' && (i == 0 || wielomian2[i - 1] == '+'))
+                {
+                    outputString2 += "-1";
+                }
+                else if (wielomian2[i] == '+' || wielomian2[i] == '-')
+                {
+                    outputString2 += wielomian2[i];
+                }
+                else
+                {
+                    outputString2 += wielomian2[i];
+                }
+            }
+            ExtendedArithmetic.Polynomial asd = Polynomial.Parse(outputString1);
+            ExtendedArithmetic.Polynomial assd = Polynomial.Parse(outputString2);
+
+            Polynomial wynik = ExtendedArithmetic.Polynomial.Divide(asd, assd);
+            String qwe = wynik.ToString();
+
+            resultLabel.Text = "Wynik: " + qwe;
         }
 
         private void secondPolynominalText_TextChanged(object sender, EventArgs e)
@@ -123,161 +279,32 @@ namespace polynomial_calculator
         private void resultLabel_Click(object sender, EventArgs e)
         {
             string result = resultLabel.Text;
-            //write in console the result of the operation 
-
-
-            //Console.WriteLine(resultStr);
-
-
 
             //serialize result to json
-            //string json = JsonConvert.SerializeObject(result, Formatting.Indented);
-            //File.WriteAllText("result.json", json);
+            string json = JsonConvert.SerializeObject(result, Formatting.Indented);
+            File.WriteAllText("result.json", json);
 
-            //string fullPath = Path.GetFullPath("result.json");
-            //File.WriteAllText(fullPath, json);
+            string fullPath = Path.GetFullPath("result.json");
+            File.WriteAllText(fullPath, json);
 
-            //Console.WriteLine($"Zapisano plik {"result.json"} w {fullPath}");
-            ////deserialize json to object
-            //string jsonFromFile = File.ReadAllText("result.json");
-            //string resultFromFile = JsonConvert.DeserializeObject<string>(jsonFromFile);
+            Console.WriteLine($"Zapisano plik {"result.json"} w {fullPath}");
+            //deserialize json to object
+            string jsonFromFile = File.ReadAllText("result.json");
+            string resultFromFile = JsonConvert.DeserializeObject<string>(jsonFromFile);
 
-            ////display results in label2 
-            //label2.Text = resultFromFile;
+            //display results in label2 
+            label2.Text = resultFromFile;
 
         }
 
-        private List<Term> ParsePolynomial(string polynomial)
-        {
-            List<Term> terms = new List<Term>();
 
-            // Podziel wielomian na pojedyncze wyrazy
-            string minus = polynomial.Substring(0, 1);
-            string[] tokens = polynomial.Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
-
-            // Dla każdego wyrazu wykonaj następujące kroki:
-            foreach (string token in tokens)
-            {
-                string termStr = token.Trim();
-
-                // Sprawdź znak przed wyrazem (dodawanie lub odejmowanie)
-                int sign = 1;
-                if (termStr.StartsWith("-") || minus == "-")
-                {
-                    sign = -1;
-                    //termStr = termStr.Substring(1);
-                }
-                else if (termStr.StartsWith(minus))
-                {
-                    termStr = termStr.Substring(1);
-                }
-
-                // Podziel wyraz na współczynnik i wykładnik
-                int coef, exp;
-                if (termStr.EndsWith("x"))
-                {
-
-                    if (termStr.Length > 1)
-                        coef = int.Parse(termStr.Substring(0, termStr.Length - 1));
-                    else
-                        coef = 1;
-
-                    exp = 1;
-                }
-                else if (termStr.Contains("x^"))
-                {
-                    string[] parts = termStr.Split(new string[] { "x^" }, StringSplitOptions.None);
-
-                    if (parts[0] == "")
-                    {
-                        parts[0] = "1";
-                    }
-
-                    coef = int.Parse(parts[0]);
-                    exp = int.Parse(parts[1]);
-                }
-                else
-                {
-                    //if the numbers are in integer format add them normally 
-                    coef = 1;
-                    exp = int.Parse(termStr);
-
-
-                    //coef = int.Parse(termStr);
-                    //exp = 0;
-                }
-
-                // Dodaj nowy wyraz do listy
-                terms.Add(new Term(sign * coef, exp));
-            }
-
-            return terms;
-        }
-
-        private List<Term> AddPolynomials(List<Term> poly1, List<Term> poly2)
-        {
-            List<Term> result = new List<Term>();
-
-            // Połącz obie listy, aby uzyskać wszystkie wyrazy
-            List<Term> allTerms = poly1.Concat(poly2).ToList();
-
-            // Zgrupuj wyrazy o tym samym wykładniku i dodaj ich współczynniki
-            var groupedTerms = allTerms.GroupBy(t => t.Exponent);
-            foreach (var group in groupedTerms)
-            {
-                int sum = group.Sum(t => t.Coefficient);
-                if (sum != 0)
-                {
-                    result.Add(new Term(sum, group.Key));
-                }
-            }
-
-            return result;
-        }
-
-        private string ConvertPolynomialToString(List<Term> polynomial)
-        {
-            // Sortuj wyrazy względem wykładnika malejąco
-            polynomial = polynomial.OrderByDescending(t => t.Exponent).ToList();
-
-            // Konwertuj wyrazy na łańcuchy znaków
-            List<string> termStrings = polynomial.Select(t => t.ToString()).ToList();
-
-            // Połącz łańcuchy znaków z wyrazami za pomocą znaku plus
-            string result = string.Join(" + ", termStrings);
-
-            // Jeśli wynik ma znak minus na początku, usuń go
-            //if (result.StartsWith("-"))
-            //{
-            //    result = result.Substring(1);
-            //}
-
-            return result;
-        }
     }
 
-    public class Term
-    {
-        public int Coefficient { get; set; }
-        public int Exponent { get; set; }
 
-        public Term(int coef, int exp)
-        {
-            Coefficient = coef;
-            Exponent = exp;
-        }
-
-        public override string ToString()
-        {
-            if (Coefficient == 0)
-            {
-                return "0";
-            }
-
-            string coefStr = (Coefficient > 0) ? Coefficient.ToString() : $"-{Math.Abs(Coefficient)}";
-            string expStr = (Exponent == 0) ? "" : $"x^{Exponent}";
-
-            return $"{coefStr}{expStr}";
-        }
-    }
 }
+
+
+
+
+
+
